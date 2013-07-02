fe86ad14cc0a0b28873af367149de7c08bcb54de
Michael Bayne <mdb@samskivert.com>
2013-07-02 10:31:15 -0700

    Tidy up series screen UI.

--- core/src/main/scala/everything/SeriesScreen.scala
+++ core/src/main/scala/everything/SeriesScreen.scala
@@ -17,24 +17,25 @@ class SeriesScreen (game :Everything, who :PlayerName, path :Array[String], catI
   def this (game :Everything, who :PlayerName, path :Array[String], scard :SeriesCard) =
     this(game, who, path :+ scard.name, scard.categoryId)
 
-  val cache = new UI.ImageCache(game)
-  val cardsEnabled = Value.create(true :JBoolean)
+  val cbox = UI.stretchBox()
+  val getSeries = game.gameSvc.getSeries(who.userId, catId)
+  onShown.connect(unitSlot {
+    getSeries.onFailure(onFailure).onSuccess(slot { series =>
+      val cache = new UI.ImageCache(game)
+      val cardsEnabled = Value.create(true :JBoolean)
+      val cards = new Group(new TableLayout(4).gaps(10, 10), Style.VALIGN.top)
+      series.things.foreach(tc => {
+        val status = if (tc == null) SlotStatus.UNFLIPPED else SlotStatus.FLIPPED
+        cards.add(new CardButton(game, this, cache, cardsEnabled).update(status, tc))
+      })
+      cbox.set(UI.vscroll(cards))
+    })
+  }).once()
 
   override def createUI () {
-    val cards = new Group(new TableLayout(4).gaps(10, 10), Style.VALIGN.top)
-    cards.add(TableLayout.colspan(new Label("Loading..."), 4))
-    root.add(header(path(0)),
-             UI.plate(UI.icon(UI.frameImage(UI.friendImage(who), 50, 50)),
-                      UI.headerLabel(who.toString), UI.pathLabel(path.drop(1))),
-             AxisLayout.stretch(UI.vscroll(cards)))
-
-    game.gameSvc.getSeries(who.userId, catId).onFailure(onFailure).
-      onSuccess(slot { series =>
-        cards.removeAll()
-        series.things.foreach(tc => {
-          val status = if (tc == null) SlotStatus.UNFLIPPED else SlotStatus.FLIPPED
-          cards.add(new CardButton(game, this, cache, cardsEnabled).update(status, tc))
-        })
-      })
+    root.add(headerPlate(UI.icon(UI.frameImage(UI.friendImage(who), 36, 36)),
+                         UI.headerLabel(who.toString), UI.pathLabel(path.dropRight(1))),
+             UI.headerLabel(path.last),
+             cbox.set(new Label("Loading...")))
   }
 }
