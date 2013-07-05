4e52b879a3ddc7e79187aadafc830563ab062026
Michael Bayne <mdb@samskivert.com>
2013-07-05 15:35:38 -0700

    A shitton of work on fancy collection screen animations.
    It's pretty good now, but there are sometimes edge cases which are hard to deal
    with. Maybe there's a better approach to smooth expandy contracty lists that
    would somehow magically deal with these edge cases, but I'll have to think
    about it. The approach I settled on is way simpler/more robust than a few early
    ones I tried.

--- core/src/main/scala/everything/CollectionScreen.scala
+++ core/src/main/scala/everything/CollectionScreen.scala
@@ -6,6 +6,7 @@ package everything
 
 import scala.annotation.tailrec
 import scala.collection.JavaConversions._
+import scala.collection.mutable.{Set => MSet}
 
 import playn.core.PlayN._
 import playn.core._
@@ -16,6 +17,7 @@ import tripleplay.anim.Flicker
 import tripleplay.ui._
 import tripleplay.ui.layout.AxisLayout
 import tripleplay.ui.layout.TableLayout
+import tripleplay.util.Interpolator
 
 import com.threerings.everything.data._
 
@@ -59,24 +61,44 @@ class CollectionScreen (game :Everything, who :PlayerName) extends EveryScreen(g
 
     layer.setHitTester(UI.absorber)
 
-    abstract class FloatingLabel (text :String, parent :FloatingLabel, val next :FloatingLabel) {
+    abstract class FloatingLabel (val text :String, parent :ParentLabel, val next :FloatingLabel) {
       val image = UI.collectCfg.toImage(text)
 
       def left :Float = if (parent == null) 0 else parent.left + 15
-      def height :Float = image.height + next.height
       def last :FloatingLabel = if (next == nil) this else next.last
 
-      var curY = 0f
+      def selfHeight :Float = image.height
+      def height :Float = selfHeight + next.height
+      def nextHeight :Float =
+        if (next != nil) next.image.height
+        else if (parent != null) parent.nextHeight
+        else 0
+
+      var layY = 0f
       def layout (y :Float) :Float = next.layout(layoutSelf(y))
       protected def layoutSelf (y :Float) :Float = {
-        curY = y
+        layY = y
         y + image.height
       }
 
-      def render (surf :Surface, y :Float) :Float = next.render(surf, renderSelf(surf, left, y))
-      protected def renderSelf (surf :Surface, x :Float, y :Float) :Float = {
+      def render (surf :Surface, y :Float, maxy :Float) :Float = {
+        if (y >= maxy) y
+        else {
+          surf.setAlpha(math.min(1, (maxy-y)/image.height))
+          val ny = renderSelf(surf, left, y, maxy)
+          surf.setAlpha(1)
+          next.render(surf, ny, maxy)
+        }
+      }
+
+      protected def renderSelf (surf :Surface, x :Float, y :Float, maxy :Float) :Float = {
         surf.drawImage(image, x, y)
-        // TODO: animate ourselves into this position instead of jumping right there
+        if (parent != null) {
+          val pl = parent.left
+          val hh = image.height/2
+          surf.drawLine(pl+7, y+hh, pl+13, y+hh, 1)
+          parent.childRenderedAt(this, y)
+        }
         y + image.height
       }
 
@@ -106,54 +128,88 @@ class CollectionScreen (game :Everything, who :PlayerName) extends EveryScreen(g
     }
 
     abstract class ParentLabel (
-      text :String, kids :Int, parent :FloatingLabel, next :FloatingLabel, sel :Selection
+      text :String, kidCount :Int, parent :ParentLabel, next :FloatingLabel, sel :Selection
     ) extends FloatingLabel(text, parent, next) {
 
-      def makeChildren :FloatingLabel
+      val numImg = UI.statusCfg.toImage(kidCount.toString)
       lazy val children = makeChildren
       lazy val lastChild = children.last
+
       var childrenShowing = false
-      val cimage = UI.statusCfg.toImage(kids.toString)
+      var childrenHeight = 0f
+      var refChildrenHeight = 0f
+      var animAccum = 0f
+      var animTime = 0f
+      var lastChildBot = 0f
+
+      def makeChildren :FloatingLabel
 
       def select (selected :Boolean) {
-        // TODO: animate
-        childrenShowing = selected
+        if (childrenShowing) {
+          childrenShowing = false
+        } else {
+          childrenShowing = true
+          if (selected) scrollTarget = this
+        }
+        refChildrenHeight = children.height
+        animAccum = 0
+        animTime = math.min(refChildrenHeight*2, 500) // .5 "pixel"/ms
+        noteAnimating(this, true)
       }
 
-      override def height = super.height + (if (childrenShowing) children.height else 0)
+      def childRenderedAt (label :FloatingLabel, y :Float) {
+        if (label == lastChild) {
+          lastChildBot = math.min(lastChildBot, y + label.image.height/2)
+        }
+      }
+
+      override def selfHeight = super.selfHeight + (if (childrenShowing) children.height else 0)
 
       override protected def layoutSelf (y :Float) = {
         val bot = super.layoutSelf(y)
         if (childrenShowing) children.layout(bot) else bot
       }
 
-      override protected def renderSelf (surf :Surface, x :Float, y :Float) = {
-        val ny = super.renderSelf(surf, x, y)
+      override protected def renderSelf (surf :Surface, x :Float, y :Float, maxy :Float) = {
+        val ny = super.renderSelf(surf, x, y, maxy)
 
-        if (!childrenShowing) {
-          surf.drawImage(cimage, width-10-cimage.width, y+(image.height-cimage.height)/2)
-          ny
-
-        } else {
-          val cy = children.render(surf, ny)
-          val hh = image.height/2
-          val lh = lastChild.curY + flicker.position
-          // render the lines connecting us to our children
-          surf.drawLine(left+7, ny-5, left+7, lh+hh, 1)
-          var child = children
-          while (child != nil) {
-            val ch = child.curY + flicker.position
-            surf.drawLine(left+7, ch+hh, left+13, ch+hh, 1)
-            child = child.next
+        // if our children are animating, update them
+        val ch = if (animTime == 0) childrenHeight else {
+          animAccum = math.min(animTime, animAccum + frameDt)
+          val animPct = animAccum / animTime
+          val newCH = refChildrenHeight * (if (childrenShowing) animPct else 1-animPct)
+          if (parent != null) {
+            parent.childrenHeight -= childrenHeight
+            parent.childrenHeight += newCH
           }
-          cy
+          if (animAccum == animTime) {
+            animTime = 0
+            noteAnimating(this, false)
+          }
+          childrenHeight = newCH
+          newCH
         }
+
+        // fade our count in/out as our children are hidden/shown
+        val numAlpha = 1 - math.max(0, ch / 10)
+        if (numAlpha > 0) surf.setAlpha(numAlpha).
+          drawImage(numImg, width-10-numImg.width, y+(image.height-numImg.height)/2).
+          setAlpha(1)
+
+        // render whatever part of our children is visible
+        val bot = math.min(maxy, ny + ch)
+        lastChildBot = bot
+        children.render(surf, ny, bot)
+
+        // render the line connecting us to our children
+        if (ch > 0 && lastChildBot > ny-5) surf.drawLine(left+7, ny-5, left+7, lastChildBot, 1)
+        ny + ch
       }
 
       override def onClick (x :Float, y :Float) = {
-        if (!childrenShowing) super.onClick(x, y)
+        if (childrenHeight == 0) super.onClick(x, y)
         else {
-          val eheight = image.height + children.height
+          val eheight = selfHeight
           if (y > eheight) next.onClick(x, y-eheight)
           else if (y > image.height) children.onClick(x, y-image.height)
           else super.onClick(x, y)
@@ -161,7 +217,6 @@ class CollectionScreen (game :Everything, who :PlayerName) extends EveryScreen(g
       }
 
       override def onTap () {
-        trigger = this
         sel.set(this)
         invalidate()
       }
@@ -170,7 +225,7 @@ class CollectionScreen (game :Everything, who :PlayerName) extends EveryScreen(g
     val nil = new FloatingLabel("", null, null) {
       override def height = 0
       override def layout (y :Float) = y
-      override def render (surf :Surface, y :Float) = y
+      override def render (surf :Surface, y :Float, maxy :Float) = y
       override def onClick (x :Float, y :Float) = false
       override def onTap () {}
     }
@@ -182,12 +237,11 @@ class CollectionScreen (game :Everything, who :PlayerName) extends EveryScreen(g
         def makeChildren = (subs :\ nil) { case ((scname, series), next) =>
           new ParentLabel(scname, series.size, this, next, openSubCat) {
             def makeChildren = (series :\ nil) { case (s, next) =>
-              // TODO: series label
               new FloatingLabel(s.name, this, next) {
                 val pie = UI.pieImage(s.owned / s.things.toFloat, 8)
-                override def renderSelf (surf :Surface, x :Float, y :Float) = {
+                override def renderSelf (surf :Surface, x :Float, y :Float, maxy :Float) = {
                   surf.drawImage(pie, x, y + image.height/2 - pie.height/2)
-                  super.renderSelf(surf, x + pie.width + 2, y)
+                  super.renderSelf(surf, x + pie.width + 2, y, maxy)
                 }
                 override def onTap () {
                   new SeriesScreen(game, who, Array(cname, scname, s.name), s.categoryId).push()
@@ -202,7 +256,7 @@ class CollectionScreen (game :Everything, who :PlayerName) extends EveryScreen(g
     val renderer = new ImmediateLayer.Renderer() {
       def render (surf :Surface) {
         surf.setFillColor(UI.textColor)
-        cats.render(surf, flicker.position)
+        cats.render(surf, flicker.position, Short.MaxValue)
       }
     }
 
@@ -214,26 +268,40 @@ class CollectionScreen (game :Everything, who :PlayerName) extends EveryScreen(g
     }
 
     var labels = graphics.createImmediateLayer(1, 1, renderer)
-    var trigger :FloatingLabel = _
+    var scrollTarget :ParentLabel = _
+    var frameDt :Float = 0
+    val active = MSet[ParentLabel]()
 
     def paint (clock :Clock) {
+      frameDt = clock.dt
       flicker.paint(clock)
+    }
+
+    def noteAnimating (label :ParentLabel, isAnim :Boolean) {
+      // disable the flicker while anything is animating
+      if (isAnim) active += label else active -= label
+      flicker.enabled.update(active.isEmpty)
     }
 
     override def layout () {
       super.layout()
       labels.destroy()
-      labels = graphics.createImmediateLayer(width.toInt, height.toInt, renderer)
+      labels = graphics.createImmediateLayer(size.width.toInt, size.height.toInt, renderer)
       layer.add(labels)
-      // make sure that the trigger label is in the same screen position after "relayout"
-      if (trigger != null) {
-        val otpos = trigger.curY
-        val bot = cats.layout(0)
-        val ntpos = trigger.curY
-        flicker.position += (otpos-ntpos)
-        flicker.min = size.height - bot
-      } else {
-        flicker.min = size.height - cats.layout(0)
+      // adjusting the min won't cause anything to render at a different position, yay!
+      flicker.min = size.height - cats.layout(0)
+      // if we have a label that wants to be scrolled into view, do that now
+      if (scrollTarget != null) {
+        val ttop = math.max(-scrollTarget.layY, flicker.min)
+        val tbot = ttop - scrollTarget.selfHeight - scrollTarget.nextHeight
+        val cbot = flicker.position - size.height
+        val tpos = math.max(ttop, if (cbot > tbot) tbot+size.height else flicker.position)
+        val npos = math.min(0, tpos) // don't scroll off the top
+        scrollTarget = null
+        // attempt to scroll all of our children into view
+        iface.animator.setValue(flicker.enabled, false :JBoolean).`then`.
+          tween(flicker.posValue).to(npos).in(500).easeInOut.`then`.
+          setValue(flicker.enabled, true :JBoolean)
       }
       // TODO: animate a scrollbar...
     }
