ed502f48e974a5ab9116c2b11361c63840dc6d61
Michael Bayne <mdb@samskivert.com>
2013-07-10 11:58:19 -0700

    Throb the 'Let's Go' button after 15s, just in case.

--- core/src/main/scala/everything/IntroScreen.scala
+++ core/src/main/scala/everything/IntroScreen.scala
@@ -10,6 +10,14 @@ class IntroScreen (game :Everything) extends EveryScreen(game) {
 
   override def createUI () {
     val arrow = "\u2b07"
+    val go = new Button("Let's Go!") {
+      override protected def layout () {
+        super.layout()
+        // move the origin of the button's layer to its center, so we can throb it
+        layer.setOrigin(size.width/2, size.height/2)
+        layer.transform.translate(size.width/2, size.height/2)
+      }
+    }.onClick(unitSlot { game.main.replace() })
     root.add(UI.shim(10, 10),
              UI.headerLabel("Welcome to\nThe Everything Game!").addStyles(Style.TEXT_WRAP.on),
              UI.shim(10, 10),
@@ -24,9 +32,11 @@ class IntroScreen (game :Everything) extends EveryScreen(game) {
              UI.glyphLabel(arrow),
              UI.headerLabel("Amass awesome collection!"),
              UI.stretchShim(),
-             UI.bgroup(UI.button("Let's Go!") {
-               game.main.replace()
-             }),
+             UI.bgroup(go),
              UI.shim(10, 10))
+    // give them 15s to read the page, then start throbbing the OK button
+    iface.animator.delay(15*1000).`then`.repeat(go.layer).
+      tweenScale(go.layer).to(1.05f).in(400).`then`.
+      tweenScale(go.layer).to(1).in(400)
   }
 }
