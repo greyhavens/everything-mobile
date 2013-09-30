763ddf4ebf4fd46f9133eeee4285a889d27a83b7
Michael Bayne <mdb@samskivert.com>
2013-09-30 11:33:34 -0700

    Finished wiring up IAB.
    This uses IABv3 which is better than v2 but still suffers from some Android
    architectural bullshit like onActivityResult blah blah.

--- android/src/main/scala/everything/EverythingActivity.scala
+++ android/src/main/scala/everything/EverythingActivity.scala
@@ -7,6 +7,10 @@ package everything
 import java.text.DateFormat
 import java.util.{Arrays, ArrayList, Date, TimeZone}
 
+import scala.collection.JavaConversions._
+
+import android.app.Activity
+import android.app.PendingIntent
 import android.content.{Context, ComponentName, Intent, ServiceConnection}
 import android.graphics.Typeface
 import android.os.{Bundle, IBinder}
@@ -16,7 +20,10 @@ import com.android.vending.billing.IInAppBillingService
 
 import playn.android.GameActivity
 import playn.core.{Font, PlayN}
-import react.{RFuture, RPromise}
+import react.{RFuture, RPromise, Slot}
+
+import com.threerings.everything.data.SessionData
+import com.threerings.everything.rpc.EveryAPI
 
 class EverythingActivity extends GameActivity {
 
@@ -38,7 +45,37 @@ class EverythingActivity extends GameActivity {
       result
     }
 
-    def buyProduct (game :Everything, sku :String) = RFuture.failure(new Exception("TODO!"))
+    def buyProduct (game :Everything, sku :String) = {
+      val result = RPromise.create[Unit]()
+      val devtok = sku + ":" + System.currentTimeMillis
+      val buyIntentBundle = billSvc.getBuyIntent(3, getPackageName, sku, "inapp", devtok)
+      buyIntentBundle.getInt("RESPONSE_CODE") match {
+        case 0 =>
+          platform.log.info(s"Starting purchase intent for $sku")
+          val pendingIntent = buyIntentBundle.getParcelable[PendingIntent]("BUY_INTENT")
+          startIntentSenderForResult(pendingIntent.getIntentSender, BuyCode, new Intent(), 0, 0, 0)
+        case code =>
+          platform.log.warn(s"getBuyIntent($sku) failed: $code")
+          result.fail(new Exception(s"Android Billing error $code"))
+      }
+      result
+    }
+
+    def purchaseRedeemed (sku :String, orderId :String) {
+      val purchs = readPurchases(null)
+      purchs find(_.orderId == orderId) match {
+        case Some(purch) =>
+          platform.log.info(s"Consuming purchase $purch")
+          platform.invokeAsync(new Runnable() {
+            def run = {
+              val rv = billSvc.consumePurchase(3, getPackageName, purch.purchaseToken)
+              if (rv != 0) platform.log.warn(s"consumePurchase($orderId) fail: $rv")
+            }
+          })
+        case None =>
+          platform.log.warn(s"consumePurchase($orderId) found no such order [have=$purchs]")
+      }
+    }
   }
 
   val facebook = new DroidBook(this)
@@ -47,11 +84,16 @@ class EverythingActivity extends GameActivity {
   // android billing stuffs; yay for mutability!
   var billSvc :IInAppBillingService = _
   var billConn = new ServiceConnection() {
-    override def onServiceDisconnected(name :ComponentName) {
+    override def onServiceDisconnected (name :ComponentName) {
       billSvc = null
     }
-    override def onServiceConnected(name :ComponentName, service :IBinder) {
+    override def onServiceConnected (name :ComponentName, service :IBinder) {
       billSvc = IInAppBillingService.Stub.asInterface(service)
+      // re-redeem any lingering unconsumed purchases when our session is refreshed
+      game.sess.connectNotify(new Slot[SessionData]() {
+        def onEmit (sess :SessionData) =
+          if (sess != null) readPurchases(null) foreach redeemPurchase("linger")
+      })
     }
   }
 
@@ -72,7 +114,20 @@ class EverythingActivity extends GameActivity {
 
   override def onActivityResult (requestCode :Int, resultCode :Int, data :Intent) {
     super.onActivityResult(requestCode, resultCode, data)
-    facebook.onActivityResult(requestCode, resultCode, data)
+    requestCode match {
+      case BuyCode =>
+        val responseCode = data.getIntExtra("RESPONSE_CODE", 0) // TODO ???
+        resultCode match {
+          case Activity.RESULT_OK =>
+            toPurchase(data.getStringExtra("INAPP_PURCHASE_DATA"),
+                       data.getStringExtra("INAPP_DATA_SIGNATURE")) foreach redeemPurchase("bought")
+          case _ => // TODO: do we need to inform the user when billing unavailable?
+            platform.log.info(s"Non-success purchase result: $resultCode")
+        }
+
+      case _ =>
+        facebook.onActivityResult(requestCode, resultCode, data)
+    }
   }
 
   override def main () {
@@ -90,9 +145,15 @@ class EverythingActivity extends GameActivity {
   }
 
   override def platform = super.platform // make visible to friends
-  override def makeWindowFlags = super.makeWindowFlags & ~WindowManager.LayoutParams.FLAG_FULLSCREEN
   override def usePortraitOrientation = true
   override def logIdent = "every"
+  override def makeWindowFlags = // we want the status bar to remain visible
+    super.makeWindowFlags & ~WindowManager.LayoutParams.FLAG_FULLSCREEN
+
+  protected def redeemPurchase (source :String)(purch :Purchase) {
+    platform.log.info(s"Redeeming [$source] purchase $purch")
+    game.redeemPurchase(purch.sku, EveryAPI.PF_PLAYSTORE, purch.orderId, purch.receipt)
+  }
 
   protected def resolveProducts (result :RPromise[Seq[Product]]) {
     val querySkus = new Bundle()
@@ -100,18 +161,45 @@ class EverythingActivity extends GameActivity {
     val skuDetails = billSvc.getSkuDetails(3, getPackageName, "inapp", querySkus);
     skuDetails.getInt("RESPONSE_CODE") match {
       case 0 =>
-        val prods = skuDetails.getStringArrayList("DETAILS_LIST").toArray(Array(""))
-        result.succeed(prods flatMap toProduct)
+        result.succeed((skuDetails.getStringArrayList("DETAILS_LIST") map toProduct).flatten)
       case code =>
         result.fail(new Exception(s"Android billing request failed (code $code)"))
     }
   }
 
-  protected def toProduct (json :String) :Option[Product] = try {
+  protected def readPurchases (conToken :String) :Seq[Purchase] = {
+    val ownedItems = billSvc.getPurchases(3, getPackageName, "inapp", conToken)
+    val purchases = ownedItems.getInt("RESPONSE_CODE") match {
+      case 0 =>
+        val data = ownedItems.getStringArrayList("INAPP_PURCHASE_DATA_LIST")
+        val sigs = ownedItems.getStringArrayList("INAPP_DATA_SIGNATURE_LIST")
+        platform.log.info("Read purchases " + data + " " + sigs)
+        if (data == null || data.size == 0) Seq()
+        else (((data :Seq[String]) zip sigs) map toPurchase.tupled).flatten
+      case code =>
+        platform.log.warn(s"getPurchases($conToken) failure [code=$code]")
+        Seq()
+    }
+    ownedItems.getString("INAPP_CONTINUATION_TOKEN") match {
+      case null => purchases
+      case conToken => purchases ++ readPurchases(conToken)
+    }
+  }
+
+  case class Purchase (sku :String, orderId :String, purchaseToken :String, receipt :String)
+  protected val toPurchase = (json :String, sig :String) => try {
+    val obj = platform.json.parse(json)
+    Some(Purchase(obj.getString("productId"), obj.getString("orderId"),
+                  obj.getString("purchaseToken"), sig + "\n" + json))
+  } catch {
+    case e :Throwable => platform.log.warn(s"toPurchase($json, $sig) failure", e) ; None
+  }
+
+  protected val toProduct = (json :String) => try {
     val obj = platform.json.parse(json)
     Some(Product(obj.getString("productId"), obj.getString("price")))
   } catch {
-    case e :Throwable => platform.log.warn(s"Failed to parse product JSON [json=$json", e) ; None
+    case e :Throwable => platform.log.warn(s"toProduct($json) fail", e) ; None
   }
 
   override def scaleFactor = {
@@ -131,4 +219,5 @@ class EverythingActivity extends GameActivity {
   }
 
   private final val MillisPerMinute = 1000*60
+  private final val BuyCode = 3773
 }
